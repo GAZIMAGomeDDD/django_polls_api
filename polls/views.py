49a5da82c00dc8fcf39f5ce77c5afb624e2b4d03
Gazimagomed <battlefield3gazik@gmail.com>
2021-07-03 21:01:33 +0300

    Implement create poll function and routing

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Poll, Choice
+import json
 
-# Create your views here.
+
+@csrf_exempt
+def create_poll_view(request):
+
+    if request.method == 'POST':
+        
+        poll_text = request.POST['poll_text']
+        choices = json.loads(request.POST['choices'])
+
+        poll = Poll.objects.create(poll_text=poll_text)
+
+        for choice_text in choices:
+            
+            Choice.objects.create(
+                poll=poll,
+                choice_text=choice_text
+            )
+
+        return HttpResponse('Good!')
+
+    return HttpResponse('Not Implemented')
