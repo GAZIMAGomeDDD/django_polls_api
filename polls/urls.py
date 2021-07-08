91bc43ae7072536c67b30ae527214d72c10ad87b
Gazimagomed <battlefield3gazik@gmail.com>
2021-07-08 17:57:19 +0300

    Fix url "/createPoll/"

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@ from .views import CreatePollViewSet, PollView, GetResult
 from django.urls import path
 
 urlpatterns = [
-    path('createPoll/', CreatePollViewSet.as_view({'post': 'createPoll'}), name='createPoll'),
+    path('createPoll/', CreatePollViewSet.as_view({'post': 'create'}), name='createPoll'),
     path('poll/', PollView.as_view(), name='poll'),
     path('getResult/', GetResult.as_view(), name='createPoll'),
 ]
